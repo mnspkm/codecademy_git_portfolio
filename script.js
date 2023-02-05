1f163bad753c46057db60766f10ba3f7c3eff9e8
Minsup Kim <kim.mnsp@gmail.com>
2023-02-05 15:01:08 +0900

    generate function and log the return value

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 let brandPool = ['Samsung','LG','Apple','Datadog','Amazon','NAVER',"Kakao",'Microsoft'];
-
+const pickRandomBrand = () => {
+    return brandPool[brandPool.length * Math.ceil(Math.random())];
+};
+console.log(pickRandomBrand());
